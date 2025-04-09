0bcf650beb65b87a6c608800d16060cde28d383c
Annette Lasar <146415092+Annette-Lasar@users.noreply.github.com>
2025-04-09 08:54:56 +0200

    Install AbstractUser, BusinessProfile and CustomerProfile

--- coderr_freelancer/settings.py
+++ coderr_freelancer/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'users_auth_app'
 ]
 
+AUTH_USER_MODEL = 'users_auth_app.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -122,3 +124,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:5500",
+#     "http://localhost:5500",
+#     "http://127.0.0.1:4200",
+#     "http://localhost:4200",
+# ]
+
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://127.0.01:5500",
+#     "http://localhost:5500",
+#     "http://127.0.0.1:4200",
+#     "http://localhost:4200",
+# ]
--- users_auth_app/models.py
+++ users_auth_app/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    file = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+class BusinessProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tel = models.CharField(max_length=30)
+    location = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    working_hours = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"Business: {self.user.username}"
+    
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Customer: {self.user.username}"
